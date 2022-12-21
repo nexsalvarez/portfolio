9a5a7a3673f077945452d4773c186bca221d5e4c
Nestor Alvarez <nesty07largmail.com>
2022-12-20 22:46:32 -0400

    Created the Project Class as JS object

---
+++ src/JS/main.js
@@ -0,0 +1,2 @@
+const servicointi = new Project ('Personal', 'Servicointi', 'feb 2022', './src/img/desktop-capture-servicointi.png', 'WordPress', 'La página web de la empresa familiar, Servicointi, fue creada por mí utilizando las herramientas de WordPress', 'https://servicointi.com/');
+servicointi.createProject();
---
+++ src/JS/projects.js
@@ -0,0 +1,41 @@
+const agencyProjects = document.querySelector('.agency-projects-container');
+const personalProjects = document.querySelector('.personal-projects-container');
+const dataProjects = document.querySelector('.data-projects-container');
+
+class Project {
+    constructor (work, name, date, img, type, description, url) {
+        this.work = work;
+        this.name = name;
+        this.date = date;
+        this.img = img;
+        this.type = type;
+        this.description = description;
+        this.url = url;
+    }
+    createProject () {
+        let newProject = document.createElement('div');
+        newProject.classList.add('project-card')
+
+        let view = `<picture>
+        <img src="${this.img}" alt="${this.name}">
+        </picture>
+        <p class="project--type">${this.type}</p>
+        <h3 class="project--title">${this.name}</h3>
+        <p class="project--description">${this.description} <span class="project--date">${this.date}</span></p>
+        <a href="${this.url}" target="_blank" class="project--url"><p>Observa el proyecto</p></a>
+        `
+        let newViewProject = function () {
+            return newProject.innerHTML = view;
+        };
+
+        if (this.work == 'Agency') {
+            return agencyProjects.innerHTML = newViewProject();
+        } else if (this.work == 'Personal') {
+            return personalProjects.innerHTML = newViewProject();
+        } else if (this.work == 'Data') {
+            return dataProjects.innerHTML = newViewProject();
+        } else {
+            return console.log("Tipo de proyecto inválido");
+        }
+    }
+}
