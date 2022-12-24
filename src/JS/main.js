1709e7883f1f2fddc8493d094cf594ca8798e304
Nestor Alvarez <nesty07largmail.com>
2022-12-23 22:55:45 -0400

    Resolve problem with project section JS

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,6 +2,9 @@ const spanTitle = document.querySelector('#words');
 const agencyButton = document.querySelector('#agency-button');
 const personalButton = document.querySelector('#personal-button');
 const dataButton = document.querySelector('#data-button');
+const agencyProjects = null || document.querySelector('.agency-projects-container');
+const personalProjects = null || document.querySelector('#personal-projects-id');
+const dataProjects = null || document.querySelector('#data-projects-id');
 
 const wordsTitle = ["nexsalvarez", "developer", "data analyst", "Nestor", "front-end"];
 let wordsTitleContent = spanTitle.innerHTML;
@@ -34,7 +37,94 @@ setInterval (function() {
     
 }, 150);
 
-const servicointi = new Project ('Personal', 'Servicointi', 'Feb 2022', './src/img/desktop-capture-servicointi.png', 'WordPress', 'La página web de la empresa familiar, Servicointi, fue creada por mí utilizando las herramientas de WordPress.', 'https://servicointi.com/');
 
-servicointi.createProject();
+function viewAgencyProjects() {
+  let filterAgency = projects.filter(project => project.work == 'Agency');
 
+  let workAgency = `
+    ${filterAgency.map(project => 
+      `<div class="project-card">
+      <picture>
+      <img src="${project.img}" alt="${project.name}">
+      </picture>
+      <p class="project--type work-wp">${project.type}</p>
+      <h3 class="project--title">${project.name}</h3>
+      <p class="project--description">${project.description} <span class="project--date">${project.date}</span></p>
+      <a href="${project.url}" target="_blank" class="project--url">Observa el proyecto</a>
+      </div>
+    `).join(' ')}
+  `;
+  
+  return agencyProjects.innerHTML = workAgency;
+}
+
+viewAgencyProjects();
+
+function viewPersonalProjects() {
+  let filterPersonal = projects.filter(project => project.work == 'Personal');
+
+  let workPersonal = `
+    ${filterPersonal.map(project => 
+      `<div class="project-card">
+      <picture>
+      <img src="${project.img}" alt="${project.name}">
+      </picture>
+      <p class="project--type work-code">${project.type}</p>
+      <h3 class="project--title">${project.name}</h3>
+      <p class="project--description">${project.description} <span class="project--date">${project.date}</span></p>
+      <a href="${project.url}" target="_blank" class="project--url">Observa el proyecto</a>
+      </div>
+    `).join(' ')}
+  `;
+  
+  return personalProjects.innerHTML = workPersonal;
+}
+
+function viewDataProjects () {
+  let filterData = projects.filter(project => project.work == 'Data');
+
+  let workData = `
+    ${filterData.map(project => 
+      `<div class="project-card">
+      <picture>
+      <img src="${project.img}" alt="${project.name}">
+      </picture>
+      <p class="project--type work-data">${project.type}</p>
+      <h3 class="project--title">${project.name}</h3>
+      <p class="project--description">${project.description} <span class="project--date">${project.date}</span></p>
+      <a href="${project.url}" target="_blank" class="project--url">Observa el proyecto</a>
+      </div>
+    `).join(' ')}
+  `;
+
+  return dataProjects.innerHTML = workData;
+}
+
+
+agencyButton.addEventListener('click', () => {
+  personalProjects.classList.replace('personal-projects-container', 'inactive');
+  dataProjects.classList.replace('data-projects-container', 'inactive');
+  agencyProjects.classList.replace('inactive', 'agency-projects-container');
+  viewAgencyProjects();
+  personalButton.classList.remove('active-btn');
+  dataButton.classList.remove('active-btn');
+  agencyButton.classList.add('active-btn');
+});
+personalButton.addEventListener('click', () => {
+  agencyProjects.classList.replace('agency-projects-container', 'inactive');
+  dataProjects.classList.replace('data-projects-container', 'inactive');
+  personalProjects.classList.replace('inactive', 'personal-projects-container');
+  viewPersonalProjects();
+  agencyButton.classList.remove('active-btn');
+  dataButton.classList.remove('active-btn');
+  personalButton.classList.add('active-btn');
+});
+dataButton.addEventListener('click', () => {
+  agencyProjects.classList.replace('agency-projects-container', 'inactive');
+  personalProjects.classList.replace('personal-projects-container', 'inactive');
+  dataProjects.classList.replace('inactive', 'data-projects-container');
+  viewDataProjects();
+  agencyButton.classList.remove('active-btn');
+  personalButton.classList.remove('active-btn');
+  dataButton.classList.add('active-btn');
+});
--- src/JS/projects.js
+++ src/JS/projects.js
@@ -1,42 +1,29 @@
-const agencyProjects = document.querySelector('.agency-projects-container');
-const personalProjects = document.querySelector('.personal-projects-container');
-const dataProjects = document.querySelector('.data-projects-container');
-
-class Project {
-    constructor (work, name, date, img, type, description, url) {
-        this.work = work;
-        this.name = name;
-        this.date = date;
-        this.img = img;
-        this.type = type;
-        this.description = description;
-        this.url = url;
+let projects = [
+    {
+      'work': 'Agency',
+      'name': 'Servicointi',
+      'date': 'Feb 2022',
+      'img': './src/img/desktop-capture-servicointi.png',
+      'type': 'WordPress',
+      'description': 'La página web de la empresa familiar, Servicointi, fue creada por mí utilizando las herramientas de WordPress.',
+      'url': 'https://servicointi.com/'
+    },
+    {
+      'work': 'Personal',
+      'name': 'ServicointiDos',
+      'date': 'Feb 2022',
+      'img': './src/img/desktop-capture-servicointi.png',
+      'type': 'Código',
+      'description': 'La página web de la empresa familiar, Servicointi, fue creada por mí utilizando las herramientas de WordPress.',
+      'url': 'https://servicointi.com/'
+    },
+    {
+      'work': 'Data',
+      'name': 'ServicointiTres',
+      'date': 'Feb 2022',
+      'img': './src/img/desktop-capture-servicointi.png',
+      'type': 'Data',
+      'description': 'La página web de la empresa familiar, Servicointi, fue creada por mí utilizando las herramientas de WordPress.',
+      'url': 'https://servicointi.com/'
     }
-    createProject () {
-        let newProject = document.createElement('div');
-        newProject.classList.add('project-card')
-
-        let view = `<div class="project-card">
-        <picture>
-        <img src="${this.img}" alt="${this.name}">
-        </picture>
-        <p class="project--type">${this.type}</p>
-        <h3 class="project--title">${this.name}</h3>
-        <p class="project--description">${this.description} <span class="project--date">${this.date}</span></p>
-        <a href="${this.url}" target="_blank" class="project--url">Observa el proyecto</a>
-        </div>`
-        let newViewProject = function () {
-            return newProject.innerHTML = view;
-        };
-
-        if (this.work == 'Agency') {
-            return agencyProjects.innerHTML = newViewProject();
-        } else if (this.work == 'Personal') {
-            return personalProjects.innerHTML = newViewProject();
-        } else if (this.work == 'Data') {
-            return dataProjects.innerHTML = newViewProject();
-        } else {
-            return console.log("Tipo de proyecto inválido");
-        }
-    }
-}
+  ];
